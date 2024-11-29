7c7d2b4943fe1fcaf5c44e66e86d517585982650
bcaacake <128385646+bcaacake@users.noreply.github.com>
2024-11-29 22:07:01 +0500

    Лабораторная работа №1
    Тема: «Расчёты арифметических выражений в C#»

---
+++ Tyuiu.FominPT.Lab1.V20/Tyuiu.FominPT.Lab1.V20.Lib/DataService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.FominPT.Lab1.V20.Lib
+{
+    public class DataService
+    {
+        public double Calculate(double x, double y, double a)
+        {
+            double res = 8 * a
+                + (2 * y + x - 3 * a) / (a * x - 8 * y)
+                + (((2 + a) / (3 * y) * 3 + (a / y))
+                / ((3 - a) / (x + Math.Pow(y, 3))));
+            res = Math.Round(res, 3);
+            return res;
+        }
+    }
+}
---
+++ Tyuiu.FominPT.Lab1.V20/Tyuiu.FominPT.Lab1.V20.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.FominPT.Lab1.V20.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.FominPT.Lab1.V20.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e7442e8c-49e6-407b-a105-f4f24231d2e1")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
